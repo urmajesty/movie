143bf07849def7de3bef23a10ca39fc9d08d59b0
github username <github email address>
2020-01-04 17:54:46 -0600

    working out issues with api

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(() => {
+
+    $('#searchForm').on('submit', (e) => {
+        let searchText = $('#searchText').val();
+        getMovies(searchText);
+        e.preventDefault();
+    });
+});
+
+function getMovies(searchText){
+    axios.get('http://www.omdbapi.com/?s='+searchText+'&apikey=eae9843b')
+    .then((response) => {    
+    console.log(response);
+
+
+    })
+
+    .catch((err) => {
+        console.log(err);
+    });
+}
